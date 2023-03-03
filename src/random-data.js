0d9796debf2f39aaae1a9fc5eb018f1da4f101b7
Gosso-Gosso Maximillem <48509702+DUMBALINYOLO@users.noreply.github.com>
2023-03-03 16:17:54 +0200

    Initial commit

---
+++ src/random-data.js
@@ -0,0 +1,11 @@
+function genRandomTree(N = 9, reverse = false) {
+  return {
+    nodes: [...Array(N).keys()].map((i) => ({ id: i })),
+    links: [...Array(N).keys()]
+      .filter((id) => id)
+      .map((id) => ({
+        [reverse ? "target" : "source"]: id,
+        [reverse ? "source" : "target"]: Math.round(Math.random() * (id - 1))
+      }))
+  };
+}
